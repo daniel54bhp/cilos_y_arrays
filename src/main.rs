76fbbf40cb298ec356344d67bc6f98670cf2056c
daniel54bhp <paukarvv@gmail.com>
2024-03-26 10:14:24 -0400

    For y Arrays
    Aprendiendo Rust

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let mut nombres : Vec<String> = Vec::new();
+ 
+    for i in 0..3 {
+        println!("Introduce un Nombre: ");
+        let mut nombre = String::new();
+        std::io::stdin().read_line(&mut nombre).unwrap();
+    
+        nombres.push(nombre);
+      
+    }
+    for nombre in nombres {
+        println!("El nombre es: {}", nombre);
+    }
+
+    let hola = ["H","O","L","A"];
+    println!("{}", hola[0]);
+    println!("{}", hola[1]);
+    println!("{}", hola[2]);
+    println!("{}", hola[3]);
+}
